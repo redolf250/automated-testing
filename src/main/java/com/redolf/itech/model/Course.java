849ae01124bbb959c25f07ec0454ab6f7ff694be
redolf250 <redolfkendrick@gmail.com>
2022-11-14 10:57:28 +0000

    initial commit

---
+++ src/main/java/com/redolf/itech/model/Course.java
@@ -0,0 +1,14 @@
+package com.redolf.itech.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    private String courseId;
+}
